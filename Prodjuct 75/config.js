c9fc0751c0d467e8554c0ea0f4648cfcd3e1db27
sriya-sn <81259869+sriya-sn@users.noreply.github.com>
2023-07-01 15:19:55 -0400

    Add files via upload

---
+++ Prodjuct 75/config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCd6oy04VNxK6VEL3Xhe38aXDsVpbCCgI",
+  authDomain: "e-ride-stage-4-1e3fc.firebaseapp.com",
+  projectId: "e-ride-stage-4-1e3fc",
+  storageBucket: "e-ride-stage-4-1e3fc.appspot.com",
+  messagingSenderId: "56402817316",
+  appId: "1:56402817316:web:9bee4e75916830801e83b1"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
